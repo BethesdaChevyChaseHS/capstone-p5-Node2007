ecb56113cd1eef44155c8c04a8c7cbdfe0efd7f4
Nebiyou D <nebruto1234@gmail.com>
2025-04-08 11:52:33 +0000

    Updates

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+    createCanvas(800, 600);
+    setupDebugConsole();
+}
+
+function draw(){
+    background("green");
+    text("Hello World!", 400, 200);
+    textSize(35);
+    textAlign("center");
+}
