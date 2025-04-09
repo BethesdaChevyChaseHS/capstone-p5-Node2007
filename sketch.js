044969cbc598923cac20f4906ebf1bec82a6ff03
Nebiyou D <nebruto1234@gmail.com>
2025-04-09 12:10:57 +0000

    saving p5 capstone project

--- sketch.js
+++ sketch.js
@@ -1,10 +1,4 @@
 
-function setup() {
-    createCanvas(600, 800);
-    textFont("Courier New");
-    setupDebugConsole();
-}
-
 let character;
 let obstacles;
 let floor;
@@ -12,15 +6,21 @@ let floor;
 let theGameOver = false;
 let haveGameBegun = false;
 let score = 0;
+let codaFontRegular;
 
 let minDistanceBetweenObstacles = 100;
 let nextReappearDistance;
 let isUnstoppable = false;
 
+function setup() {
+    createCanvas(600, 800);
+    setupDebugConsole();
+    codaFontRegular = loadFont('Coda-Regular.ttf');
+} 
 
 function draw(){
     background("green");
-    text("Hello World!", 400, 200);
-    textSize(35);
+    text("Hello World", 200, 400);
+    textSize(32);
     textAlign("center");
 }
